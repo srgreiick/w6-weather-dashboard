e8a06781ad92ff06b69da39da82f560ec85b9d67
sgreilick <samgreilick@gmail.com>
2020-08-25 14:54:22 -0600

    added api url slot for city

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,24 @@
-var APIKey = "5c178ef223e7b2e7f72ca653df149189";
+let APIKey = "5c178ef223e7b2e7f72ca653df149189";
 
 // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
+    
+            // This function handles events where one button is clicked
+            $("#add-movie").on("click", function(event) {
+              // Preventing the buttons default behavior when clicked (which is submitting a form)
+              event.preventDefault();
+              // This line grabs the input from the textbox
+              let movie = $("#city-input").val().trim();
+      
+              // Adding the movie from the textbox to our array
+              
+      
+              // Calling renderButtons which handles the processing of our movie array
+              renderButtons();
+      
+            });
+            
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=" + APIKey;
+
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -21,7 +38,7 @@ var APIKey = "5c178ef223e7b2e7f72ca653df149189";
           $(".humidity").text("Humidity: " + response.main.humidity);
           
           // Convert the temp to fahrenheit
-          var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+          let tempF = (response.main.temp - 273.15) * 1.80 + 32;
   
           // add temp content to html
           $(".temp").text("Temperature (K) " + response.main.temp);
@@ -34,29 +51,11 @@ var APIKey = "5c178ef223e7b2e7f72ca653df149189";
         });
 
 
-        // Generic function for capturing the movie name from the data-attribute
-        function alertMovieName() {
-          var movieName = $(this).attr("data-name");
-  
-          alert(movieName);
-        }
+
   
 
   //--------------------------------------------------------------------------------------------------------------------
-        // This function handles events where one button is clicked
-        $("#add-movie").on("click", function(event) {
-          // Preventing the buttons default behavior when clicked (which is submitting a form)
-          event.preventDefault();
-          // This line grabs the input from the textbox
-          var movie = $("#city-input").val().trim();
-  
-          // Adding the movie from the textbox to our array
-          movies.push(movie);
-  
-          // Calling renderButtons which handles the processing of our movie array
-          renderButtons();
-  
-        });
+
   
         // Function for displaying the movie info
         // We're adding a click event listener to all elements with the class "movie"
