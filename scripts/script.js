522e793576d444ed5aa427f30b86bec5b0db202b
sgreilick <samgreilick@gmail.com>
2020-08-26 17:32:52 -0600

    history function will now gets that cities data"

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,11 @@ let APIKey = "5c178ef223e7b2e7f72ca653df149189";
 let city;
 let prevCities = []
 console.log(prevCities)
+
+let currentDate = moment().format(" Do MMMM");
+let now = moment().format("HH:MM")
+
+
 // Here we are building the URL we need to query the database
     
   // This function handles events where one button is clicked
@@ -29,6 +34,7 @@ $("#add-city").on("click", function(event) {
 
         // Transfer content to HTML
         $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+        $(".currentDay").text("-------- "+currentDate+" --------");
         $(".wind").text("Wind Speed: " + response.wind.speed);
         $(".humidity").text("Humidity: " + response.main.humidity);
         
@@ -55,18 +61,60 @@ $("#add-city").on("click", function(event) {
         }
       }
       for (let i = 0; i < prevCities.length; i++) {
-        $("#history").append("<p>"+prevCities[i]+"</p>");
+        $("#history").append("<p class='cityClass'>"+prevCities[i]+"</p>");
         console.log(prevCities)
       }
-      
+      $(".cityClass").on("click", function(event) {
+        cityHist(event)
       });
-
-
-    
-          });
+});
             
+});
 
+function cityHist(event) {
+            
+          
+  // Preventing the buttons default behavior when clicked (which is submitting a form)
+  event.preventDefault();
+  console.log(event.currentTarget.textContent)
+  // This line grabs the text from the history  column (FH = from history)
+  let cityFH = event.currentTarget.textContent;
+  let queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityFH+"&appid=" + APIKey;
+  //console.log(quer)
 
   
+  
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
 
+      // Log the queryURL
+      console.log(queryURL);
 
+      // Log the resulting object
+      console.log(response);
+
+      // Transfer content to HTML
+      $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+      $(".currentDay").text("-------- "+currentDate+" --------");
+      $(".wind").text("Wind Speed: " + response.wind.speed);
+      $(".humidity").text("Humidity: " + response.main.humidity);
+      
+      // Convert the temp to fahrenheit
+      let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+      // add temp content to html
+      $(".temp").text("Temperature (K) " + response.main.temp);
+      $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+      // Log the data in the console as well
+      console.log("Wind Speed: " + response.wind.speed);
+      console.log("Humidity: " + response.main.humidity);
+      console.log("Temperature (F): " + tempF);
+    
+    });
+
+}
